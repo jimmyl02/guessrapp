531853f7c59c73235fa09a53a4f0332c6f475d2c
Jimmy Li <jimmyli2002@gmail.com>
2021-07-23 16:05:58 -0700

    add push to dockerhub and modify compose to use dockerhub image

--- buildpushimage.sh
+++ buildpushimage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker build -t us-west2-docker.pkg.dev/guessrapp/app/app:0.4 .
+# build and push to gcr
+#docker build -t us-west2-docker.pkg.dev/guessrapp/app/app:0.4 .
+#gcloud auth configure-docker us-west2-docker.pkg.dev
+#docker push us-west2-docker.pkg.dev/guessrapp/app/app:0.4
 
-gcloud auth configure-docker us-west2-docker.pkg.dev
-
-docker push us-west2-docker.pkg.dev/guessrapp/app/app:0.4
+# build and push to docker hub
+docker build -t jimmyl02/guessrapp:latest .
+docker push jimmyl02/guessrapp:latest
